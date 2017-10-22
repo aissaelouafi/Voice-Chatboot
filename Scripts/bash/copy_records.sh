3420208e6a8454e330b606df3e3f43b5c31cb0a1
aissaelouafi <aissaelouafi1994@gmail.com>
2017-10-23 00:03:08 +0200

    First commit test

---
+++ Scripts/bash/copy_records.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# this script copy the new record from spool/ASTERISK/monitor to shared folder
+inotifywait -m /var/spool/asterisk/monitor -e create -e moved_to -e modify |
+	while read path action file; do
+		echo "The file '$file' apperaed in directory '$path' via '$action'"
+		cp $path$file /media/sf_Voice_Chatboot_/Records/$file
+		echo "The file '$file' copied to /media/sf_Voice_Chatboot_/Records folder"
+	done
